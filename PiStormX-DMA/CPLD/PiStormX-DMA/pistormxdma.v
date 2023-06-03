96641f52e40e910c6e4ebb1064362a44bdfbff7a
FLACO <32963547+f1ac0@users.noreply.github.com>
2023-06-03 12:35:43 +0200

    Pistorm'X DMA debut ; PiStorm'X firmware for the original board with support for Bus Arbitration signals, to hopefully work with DMA peripherals in A2000

---
+++ PiStormX-DMA/CPLD/PiStormX-DMA/pistormxdma.v
@@ -0,0 +1,262 @@
+/*
+ * Pistorm'X for Xilinx CPLD, with integrated buffers and without 200MHz clock
+ * 2022 FLACO CC-BY-NC-SA
+ * Inspired by original Pistorm, Copyright 2020 Claude Schwarz and Niklas Ekström, https://github.com/captain-amygdala/pistorm
+ */
+module pistormxdma(
+	output          PI_TXN_IN_PROGRESS, // GPIO0 //AUX0
+	output          PI_IPL_ZERO,        // GPIO1 //AUX1
+	input   [1:0]   PI_A,       // GPIO[3..2]
+//	input           PI_CLK,     // GPIO4 // Not used
+	output          PI_RESET,   // GPIO5
+	input           PI_RD,      // GPIO6
+	input           PI_WR,      // GPIO7
+	inout   [15:0]  PI_D,       // GPIO[23..8]
+
+	output  [23:1]	 M68K_A,
+	inout   [15:0]	 M68K_D,
+	input           M68K_CLK,
+//	output  [2:0]   M68K_FC,   // PU on Amiga MB // Not used
+
+	output          M68K_AS_n,  // PU on Amiga MB
+	output          M68K_UDS_n, // PU on Amiga MB
+	output          M68K_LDS_n, // PU on Amiga MB
+	output          M68K_RW,    // PU on Amiga MB
+
+	input           M68K_DTACK_n,
+//	input           M68K_BERR_n, // Not used
+
+	input           M68K_VPA_n,
+	output          M68K_E,     // No PU on Amiga MB
+	output          M68K_VMA_n, // PU on Amiga MB
+
+	input   [2:0]   M68K_IPL_n,
+
+	inout           M68K_RESET_n, // PU on Amiga MB
+	inout           M68K_HALT_n, // PU on Amiga MB
+
+	input           M68K_BR_n,
+	output          M68K_BG_n,
+	input           M68K_BGACK_n
+ );
+
+//  initial begin
+//    M68K_BG_n <= 1'b1;
+//  end
+
+  localparam REG_DATA = 2'd0;
+  localparam REG_ADDR_LO = 2'd1;
+  localparam REG_ADDR_HI = 2'd2;
+  localparam REG_STATUS = 2'd3;
+
+  wire bus_requested = !M68K_BR_n;
+  reg bus_granted = 1'b0;
+  
+  wire c7m = M68K_CLK;
+  reg M68K_VMA_nr = 1'd1;
+
+  reg [15:0] d_out;
+  reg [23:1] a_out;
+
+  reg s2=1'b0;
+  reg s3=1'b0;
+  reg s4=1'b0;
+  reg s7=1'b1; //M68K default bus state
+
+  reg [3:0] e_counter = 4'd0;
+
+  reg [2:0] ipl;
+  reg [2:0] ipl_a;
+
+//  reg st_init = 1'b0; //1=reset, 0=run
+  reg st_reset_out = 1'b1; //1=reset, 0=run
+  reg op_req = 1'b0; //1=bus operation pending
+  reg op_rw = 1'b1; //1=read, 0=write
+  reg op_a0 = 1'b0; //1=lds, 0=uds, when sz=byte
+  reg op_sz = 1'b0; //1=byte, 0=word
+
+//buffered write
+  reg [15:0] buf_d;
+  reg [23:1] buf_a;
+  reg buf_rw;
+  reg buf_a0;
+  reg buf_sz;
+
+// BUS ARBITRATION CONTROL
+  wire bgset = bus_requested & s7;
+  wire bgreset = !bus_requested & M68K_BGACK_n;
+  always @(posedge bgset, posedge bgreset) begin
+    if(bgreset)
+      bus_granted <= 1'b0;
+    else
+      bus_granted <= 1'b1;
+  end
+  
+// RESET
+  reg [1:0] resetfilter=2'b11;
+  wire oor = resetfilter==2'b01; //pulse when out of reset. delay by one clock pulse is required to prevent lock after reset
+  always @(negedge c7m) begin
+    resetfilter <= {resetfilter[0],M68K_RESET_n};
+  end
+  assign PI_RESET = st_reset_out ? 1'b1 : M68K_RESET_n;
+  assign M68K_RESET_n = st_reset_out ? 1'b0 : 1'bz;
+  assign M68K_HALT_n = st_reset_out ? 1'b0 : 1'bz;
+
+// E CLOCK
+// A single period of clock E consists of 10 MC68000 clock periods (six clocks low, four clocks high)
+  always @(negedge c7m) begin
+    if (e_counter == 4'd9)
+      e_counter <= 4'd0;
+    else
+      e_counter <= e_counter + 4'd1;
+  end
+  assign M68K_E = (e_counter > 4'd5) ? 1'b1:1'b0; //six clocks low (0-5), four clocks high (6-9)
+
+// INTERRUPT CONTROL
+  always @(negedge c7m) begin
+    ipl_a <= ~M68K_IPL_n;
+    if (ipl_a == ~M68K_IPL_n) //filter unstable signals
+      ipl <= ~M68K_IPL_n;
+  end
+  assign PI_IPL_ZERO = ipl == 3'd0;
+
+
+// PI SIDE
+
+// PI READ CYCLE
+// place the required data word on the bus when PI_RD is set. In a REG_DATA cycle, Data is latched after the falling edge of PI_TXN_IN_PROGRESS
+  assign PI_D = ((PI_A == REG_STATUS) && PI_RD) ? {ipl, 13'd0} : ((PI_A == REG_DATA && PI_RD) ? buf_d : 16'bz);
+
+// PI WRITE CYCLE
+  always @(posedge PI_WR) begin
+    case (PI_A)
+      REG_ADDR_LO: begin
+        buf_a0 <= PI_D[0];
+        buf_a[15:1] <= PI_D[15:1];
+      end
+      REG_ADDR_HI: begin
+        buf_a[23:16] <= PI_D[7:0];
+        buf_sz <= PI_D[8];
+        buf_rw <= PI_D[9];
+      end
+      REG_STATUS: begin
+//		  st_init <= PI_D[0];
+        st_reset_out <= !PI_D[1];
+      end
+    endcase
+  end
+  
+
+// Sync with 68K bus operations
+  assign PI_TXN_IN_PROGRESS = op_req;
+
+//release write early thanks to the buffer. Here in S2 however it seems there is no speed increase over release in s3 with (op_rw?s4:s3) | oor
+//release read in s4 because it is not buffered
+  wire op_reqrst= s4&op_rw | s2&!buf_rw | oor;
+  wire op_reqset= PI_WR & PI_A==REG_ADDR_HI;
+  always @(posedge op_reqset, posedge op_reqrst) begin
+    if (op_reqset)
+      op_req <= 1'b1;
+    else
+      op_req <= 1'b0;
+  end
+
+  wire d_ck = (PI_WR & PI_A==REG_DATA) | (s4 & op_rw) ;
+  always @(posedge d_ck) begin
+    if(op_rw & (s3|s4))
+      buf_d <= M68K_D;
+	else
+	  buf_d <= PI_D;
+  end
+  
+  always @(posedge s2) begin
+    a_out<=buf_a;
+    d_out<=buf_d;
+    op_a0<=buf_a0;
+    op_sz<=buf_sz;
+    op_rw<=buf_rw;
+  end
+  
+  
+//68K BUS SIDE
+
+// BUS TRANSFER STATE MACHINE
+  wire s2rst= s3 | oor;
+  wire s3rst= s4 | oor;
+  wire s4rst= s7 | oor;
+  wire s7rst= s2;
+  always @(posedge c7m, posedge s2rst) begin
+    if(s2rst)
+      s2<=1'd0;
+    else if(s7 && op_req && !bus_granted)
+      s2<=1'd1;
+  end
+  always @(negedge c7m, posedge s3rst) begin
+    if(s3rst)
+      s3<=1'd0;
+    else if(s2)
+      s3<=1'd1;
+  end
+  always @(posedge c7m, posedge s4rst) begin
+    if(s4rst)
+      s4<=1'd0;
+    else if(s3 && (!M68K_DTACK_n || (!M68K_VMA_nr && e_counter == 4'd9)) ) //S7 rise and E fall must match
+      s4<=1'd1;
+  end
+  always @(negedge c7m, posedge s7rst) begin
+    if(s7rst)
+      s7<=1'd0;
+    else if(s4 | oor)
+      s7<=1'd1;
+  end
+
+//	output [23:1]	M68K_A,
+// Entering S1, the processor drives a valid address on the address bus.
+// As the clock rises at the end of S7, the processor places the address and data buses in the high-impedance state
+  assign M68K_A = (bus_granted | (s7&!op_req)) ? 23'bz : a_out; //Z in s7 since it is the state where the Pistorm is waiting
+  
+//	inout [15:0]	M68K_D,
+// READ : On the falling edge of the clock entering state 7 (S7), the processor latches data from the addressed device
+// WRITE : During S3, the data bus is driven out of the high-impedance state as the data to be written is placed on the bus.
+// As the clock rises at the end of S7, the processor places the address and data buses in the high-impedance state
+  assign M68K_D = (bus_granted | ((s7&!op_req)|s2|op_rw)) ? 16'bz : d_out;
+
+//	output  reg [2:0] M68K_FC,
+//not supported
+
+//	output      M68K_AS,
+// On the rising edge of S2, the processor asserts AS and drives R/W low.
+// On the falling edge of the clock entering S7, the processor negates AS, UDS, or LDS
+  assign M68K_AS_n = bus_granted ? 1'bz : ((s7) ? 1'b1:1'b0);
+
+//	output      M68K_UDS,
+//	output      M68K_LDS,
+// READ : On the rising edge of state 2 (S2), the processor asserts AS and UDS, LDS, or DS
+// WRITE : At the rising edge of S4, the processor asserts UDS, or LDS // wrong: DS should be set in s3
+// On the falling edge of the clock entering S7, the processor negates AS, UDS, or LDS
+  wire op_ds_n = (s2&!op_rw)|s7;
+  assign M68K_UDS_n = bus_granted ? 1'bz : ((op_ds_n|(op_sz & op_a0)) ? 1'b1:1'b0); //disable uds when byte operation on odd address
+  assign M68K_LDS_n = bus_granted ? 1'bz : ((op_ds_n|(op_sz & !op_a0)) ? 1'b1:1'b0); //disable lds when byte operation on even address
+
+//	output      M68K_RW,
+// On the rising edge of S2, the processor asserts AS and drives R/W low.
+// As the clock rises at the end of S7, the processor drives R/W high
+  assign M68K_RW = bus_granted ? 1'bz : ((op_rw) ? 1'b1:1'b0);
+  
+//	output reg      M68K_VMA_n,
+  wire vmarst= s7 | oor;
+  always @(posedge c7m,posedge vmarst) begin
+    if(vmarst)
+      M68K_VMA_nr <= 1'b1;
+    else if(s3 && !M68K_VPA_n && e_counter == 4'd2)
+      M68K_VMA_nr <= 1'b0;
+  end
+  assign M68K_VMA_n = bus_granted ? 1'bz : (M68K_VMA_nr ? 1'b1:1'b0);
+
+//	output      M68K_BG,
+// indicates to all other potential bus master devices that the processor will relinquish bus control at the end of the current bus cycle.
+  assign M68K_BG_n = bus_requested ? 1'b0:1'b1;
+
+
+endmodule
+
---
+++ PiStormX-DMA/README.md
@@ -0,0 +1,26 @@
+# Pistorm'X with bus arbitration for DMA enabled peripherals
+While discussing with a fellow PiStorm user on the AmigaFrance Forum, I discovered that peripherals that use Direct Memory Access (DMA) on the Amiga 2000 do not work with the actual PiStorm CPU boards. Having never used a "big box" Amiga myself I did not know about this fact.
+
+Looking at the schematic of the A2000, I believe the CPU board need to support bus arbitration signals (BR, BG, BGACK) in order to float the address and data buses when other devices need them. On the PiStorm'X-68K logic, I used these signals, but as a bus master device in a reverse way to shut the real 68000 when the PiStorm emulator is active. However this means that the signals are available to the CPLD and the logic can be changed to behave like a CPU supporting DMA requests.
+
+The files in this directory are an attempt to add the support of bus arbitration signals to the first PiStorm'X CPU board (not the ones in the Beta directory).
+- They are not tested since I don't own an Amiga 2000. If you want to test it yourself on your system, feel free. If you have a spare/unused/broken one and want to make a donation to help the project, I'd be very happy.
+- The real 68000 need to be removed from the system/NOT installed on the PiStorm'X. Since the bus arbitrations are used in a normal "CPU way" this leaves now way to shut it down like the PiStorm'X 68K does. Il you install both the real 68K an the PiStorm they will use the bus at the same time, this will not work, and will probably break them. 
+
+When this is proven to work, I believe it will be great to build an A2000 CPU board with support to both the DMA devices and the real 68000.
+
+# Experimental status
+Pistorm'X is experimental, not yet well tested, and might not work with all system revisions and Pi or 68k software or provide the same performance or feature than the original. That it works for me does not mean that it will work for you. For this reason you are welcome to assemble your own and share test reports in the Discord channel named "beta-testing-pistormx".
+
+Pistorm'X-DMA is even more experimental and untested.
+
+# Making it
+The first Pistorm'X board require a few modifications and a new firmware
+- Connect the pin in _BG = CPU pin 11 to the solder jumper pad next to FC2 = CPU pin 26
+- Connect the pin in _BGACK = CPU pin 12 to the solder jumper pad next to FC1 = CPU pin 27
+- Connect the pin in _BR = CPU pin 13 to the solder jumper pad next to FC0 = CPU pin 28
+- Pins in FC0 = CPU pin 28, FC1 = CPU pin 27 and FC2 = CPU pin 26 are still not required. If you install them you need to be careful NOT to connect them to the nearby jumper pad.
+
+A board already modded for the PiStorm'X-68K will not need any modification.
+
+Build the firmware using the same settings as the Pistorm'X and flash it on the CPLD using xsvfduino, xc3sprog or your preferred method.
